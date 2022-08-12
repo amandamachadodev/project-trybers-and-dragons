a4746f6f7a54ad97566f975902e2c90ec55cf1d4
mandioquynha <machado.amandag@gmail.com>
2022-08-11 21:08:31 -0300

    fix: :sparkles: abstract class Race
    req01 concluído

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,1 +1,25 @@
-'iniciando o projeto';
+abstract class Race {
+  constructor(
+    private readonly _name: string,
+    private readonly _dexterity: number,
+  ) {
+    this._name = _name;
+    this._dexterity = _dexterity;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
